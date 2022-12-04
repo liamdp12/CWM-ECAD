51104b881daa77c9a6bbf4a1f77510b85ff889b5
Liam Perreault <liamperreault12@gmail.com>
2022-12-04 20:44:44 +0000

    Testbench for exercise 6

--- Ex6/top_tb.v
+++ Ex6/top_tb.v
@@ -7,3 +7,45 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb ();
+
+parameter CLK_PERIOD = 10;
+
+reg clk;
+reg [2:0] colour;
+reg enable;
+reg [4:0] i;
+wire [23:0] rgb;
+
+initial
+begin
+clk = 0;
+colour = 0;
+i = 0;
+enable = 1;
+forever begin
+    # (CLK_PERIOD / 2) clk = ~clk;
+end
+end
+
+initial begin
+    #30
+    colour = 1;
+    #20
+    colour = 5;
+    #30
+    colour = 7;
+    #20
+   $finish;
+end
+
+blk_mem_gen_0 converter (
+    .addra(colour),
+    .clka(clk),
+    .ena(enable),
+    .douta(rgb)
+);
+
+endmodule
